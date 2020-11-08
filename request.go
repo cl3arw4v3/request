b6b7477d3e4f9778a8043ed2997d61f27ad68eae
cl3arw4v3 <59515378+cl3arw4v3@users.noreply.github.com>
2020-11-08 16:23:41 -0300

    Update request.go
    options method added

--- request.go
+++ request.go
@@ -183,6 +183,47 @@ func Put(url string, headers map[string]string) (*Response, error) {
 
 }
 
+func Options(url string, headers map[string]string) (*Response, error) {
+
+	client := &http.Client{
+		Jar:           sessionJar,
+		CheckRedirect: func(req *http.Request, via []*http.Request) error { return http.ErrUseLastResponse },
+	}
+
+	req, err := http.NewRequest("OPTIONS", url, nil)
+
+	if err != nil {
+		return nil, fmt.Errorf("Erro ao iniciar requisição: %w", err)
+	}
+
+	for v, k := range headers {
+		req.Header.Set(v, k)
+	}
+
+	res, err := client.Do(req)
+
+	if err != nil {
+		return nil, fmt.Errorf("Erro ao finalizar requisição: %w", err)
+	}
+
+	defer res.Body.Close()
+
+	body, err := ioutil.ReadAll(res.Body)
+
+	if err != nil {
+		return nil, fmt.Errorf("Erro ao decodificar resosta da requisição: %w", err)
+	}
+
+	if res.StatusCode == http.StatusFound {
+		return &Response{res.Status, res.StatusCode, string(body), res.Header}, nil
+	}
+
+	sessionCookies = sessionJar.Cookies(req.URL)
+
+	return &Response{res.Status, res.StatusCode, string(body), res.Header}, nil
+
+}
+
 func Do(method, urli, postdata string, headers map[string]string, proxy string) (*Response, error) {
 
 	transport := &http.Transport{}
